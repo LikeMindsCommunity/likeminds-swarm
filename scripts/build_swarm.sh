1c4499d0a471cb2be134f358683a9a7f0fa32731
Mahir-G2 <mahir.gupta@likeminds.community>
2023-02-03 16:59:40 +0530

    fix in build script

--- scripts/build_swarm.sh
+++ scripts/build_swarm.sh
@@ -14,5 +14,5 @@ else
   git pull origin master
 fi
 aws s3api get-object --bucket "$APPLICATION_AWS_S3_BUCKET" --key "$APPLICATION_DOT_ENV_KEY" "$APPLICATION_DOT_ENV_LOCATION"
-wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem -P /home/ec2-user/LikeMinds-Swarm/config/
+wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem -O /home/ec2-user/LikeMinds-Swarm/config/rds-combined-ca-bundle.pem
 go build cmd/server/likeminds-swarm.go
