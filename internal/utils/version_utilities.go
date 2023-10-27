3cd7d20fabb4bd4a9d91cc383b9022ae59ef0ade
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2023-10-27 13:54:05 +0530

    Revert "[LM-10026] Enabled Edit Feature Version check for ReactJs (#120)"
    This reverts commit 9d0a7fb05679729ab8f8d0824006f2a3ba45f989.

--- internal/utils/version_utilities.go
+++ internal/utils/version_utilities.go
@@ -41,7 +41,7 @@ var EditFeedEntityVersions = map[string]int{
 	PlatformIoS:         2,
 	PlatformFlutter:     5,
 	PlatformReactNative: UnreleasedMaxVersion,
-	PlatformReactJS:     1,
+	PlatformReactJS:     UnreleasedMaxVersion,
 }
 
 // Exposed Method to check version code accessibility for a feature
