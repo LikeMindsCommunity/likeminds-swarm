24a82c3af842bbe6c89bd570dfd683758f651d59
DheerajLM <dheeraj.gurjar@likeminds.community>
2023-12-04 15:53:02 +0530

    add a function to create indexes

--- internal/services/database/mongo.go
+++ internal/services/database/mongo.go
@@ -11,10 +11,27 @@ import (
 	"github.com/nateshr/likeminds-swarm/internal/services/environment"
 	log "github.com/nateshr/likeminds-swarm/internal/services/logging"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+// Function to create indexes
+func createIndex(client *mongo.Client, dbName, collectionName string, indexFields bson.M) error {
+	collection := client.Database(dbName).Collection(collectionName)
+	indexModel := mongo.IndexModel{
+		Keys: indexFields,
+	}
+
+	_, err := collection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 
 // Internal Method to get TLS Configuration for Database Connection
 func getCustomTLSConfig(caFile string) (*tls.Config, error) {
@@ -86,6 +103,118 @@ func InitiateDB() *mongo.Database {
 		log.Info("Database(Mongo): Successfully connected and pinged.")
 
 		dbInstance = client.Database(db_name)
+
+		indexData := []bson.M{
+			{
+				"collectionName": "post",
+				"fields":         bson.M{"community_id": 1},
+			},
+			{
+				"collectionName": "post",
+				"fields":         bson.M{"is_pinned": 1},
+			},
+			{
+				"collectionName": "post",
+				"fields":         bson.M{"is_deleted": 1},
+			},
+			{
+				"collectionName": "post",
+				"fields":         bson.M{"created_at": 1},
+			},
+			{
+				"collectionName": "like",
+				"fields":         bson.M{"created_at": 1},
+			},{
+				"collectionName": "like",
+				"fields":         bson.M{"entity_id": 1},
+			},{
+				"collectionName": "like",
+				"fields":         bson.M{"is_deleted": 1},
+			},
+			{
+				"collectionName": "save",
+				"fields":         bson.M{"entity_type": 1},
+			},{
+				"collectionName": "save",
+				"fields":         bson.M{"community_id": 1},
+			},{
+				"collectionName": "save",
+				"fields":         bson.M{"saved_by": 1},
+			},{
+				"collectionName": "save",
+				"fields":         bson.M{"entity_id": 1},
+			},{
+				"collectionName": "save",
+				"fields":         bson.M{"saved_by": 1},
+			},{
+				"collectionName": "topic",
+				"fields":         bson.M{"community_id": 1},
+			},{
+				"collectionName": "topic",
+				"fields":         bson.M{"name": 1},
+			},
+			{
+				"collectionName": "topic",
+				"fields":         bson.M{"is_enabled": 1},
+			},
+			{
+				"collectionName": "pollVotes",
+				"fields":         bson.M{"poll_id": 1},
+			},{
+				"collectionName": "pollVotes",
+				"fields":         bson.M{"community_id": 1},
+			},
+			{
+				"collectionName": "customWidget",
+				"fields":         bson.M{"community_id": 1},
+			},{
+				"collectionName": "customWidget",
+				"fields":         bson.M{"parent_entity_id": 1},
+			},
+			{
+				"collectionName": "comment",
+				"fields":         bson.M{"post_id": 1},
+			},{
+				"collectionName": "comment",
+				"fields":         bson.M{"level": 1},
+			},{
+				"collectionName": "comment",
+				"fields":         bson.M{"user_id": 1},
+			},{
+				"collectionName": "comment",
+				"fields":         bson.M{"is_deleted": 1},
+			},{
+				"collectionName": "comment",
+				"fields":         bson.M{"created_at": 1},
+			},
+			{
+				"collectionName": "activity",
+				"fields":         bson.M{"community_id": 1},
+			},{
+				"collectionName": "activity",
+				"fields":         bson.M{"action_by": 1},
+			},{
+				"collectionName": "activity",
+				"fields":         bson.M{"action_on": 1},
+			},{
+				"collectionName": "activity",
+				"fields":         bson.M{"entity_id": 1},
+			},{
+				"collectionName": "activity",
+				"fields":         bson.M{"entity_type": 1},
+			},{
+				"collectionName": "activity",
+				"fields":         bson.M{"entity_owner_id": 1},
+			},
+		}
+
+		for _, indexValue := range indexData {
+			err = createIndex(client, db_name, indexValue["collectionName"].(string), indexValue["fields"].(bson.M))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
 	}
 
 	return dbInstance
