06c2816ea85e6693f46877044e25fdd2a9ad57ef
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-12-19 19:47:57 +0530

    Changed pointer variables

--- internal/api/handlers/activity.go
+++ internal/api/handlers/activity.go
@@ -178,7 +178,7 @@ func parseUserProfileActivity(handler FeedHandlers, activities []entities.Activi
 				continue
 			}
 
-			postData = activityEntityData.(requests.FetchCommentResponse).Post
+			postData = *activityEntityData.(requests.FetchCommentResponse).Post
 
 			// Update activity data
 			activity.CTA = fmt.Sprintf(utils.CommentDetailRoute, activity.EntityID.Hex(), actionByMetadata.EntityId.Hex())
--- internal/api/handlers/comment.go
+++ internal/api/handlers/comment.go
@@ -331,7 +331,7 @@ func fetchCommentData(handlers *FeedHandlers, commentId string, postId string, f
 		// Parse post response and append to Comment's post_data
 		parsePostResponse := parsePostResponse(handlers.likeHelper, handlers.commentHelper, handlers.saveHelper, handlers.topicHelper,
 			*postData, memberId, isCm, versionCode, platformCode, apiRevampV1Check, handlers.cacheHelper)
-		fetchCommentResponse.Post = parsePostResponse
+		fetchCommentResponse.Post = &parsePostResponse
 	}
 	return fetchCommentResponse, nil
 }
