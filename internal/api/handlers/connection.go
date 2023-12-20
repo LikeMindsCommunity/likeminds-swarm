cafdc2fead027338aac13843c913e6966d5a9c8d
Mahir <76210826+Mahir-G2@users.noreply.github.com>
2023-12-20 13:56:12 +0530

    [LM - 9930] Following Feed (#132)
    * Following Feed Changes
    
    * changes after pr review

---
+++ internal/api/handlers/connection.go
@@ -0,0 +1,157 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+	"github.com/nateshr/likeminds-swarm/internal/api/constants"
+	"github.com/nateshr/likeminds-swarm/internal/api/requests"
+	"github.com/nateshr/likeminds-swarm/internal/services/cache"
+	"github.com/nateshr/likeminds-swarm/internal/services/externalHelpers"
+	log "github.com/nateshr/likeminds-swarm/internal/services/logging"
+	"github.com/nateshr/likeminds-swarm/internal/utils"
+)
+
+// Internal Method to create cache key name for user connection
+func getUserConnectionCacheKeyName(userId string, communityId int) string {
+	return fmt.Sprintf(cache.UserConnnectionCacheKey, userId, utils.ConvertNumberToString(communityId))
+}
+
+// Internal Method to get User connection data from cache
+func getUserConnectionDataFromCache(handlers *FeedHandlers, userId string, communityId int) (map[string]bool, bool) {
+	userConnectionData := map[string]bool{}
+	keyExists := false
+
+	// Getting data from cache for user
+	userCacheKeyName := getUserConnectionCacheKeyName(userId, communityId)
+	get := handlers.cacheHelper.Get(userCacheKeyName)
+
+	// If key exists in cache for the User
+	if get.Err() == nil || get.Err() == redis.Nil {
+		keyExists = true
+
+		// Convert the data
+		err := get.Scan(userConnectionData)
+		if err != nil {
+			log.Error(fmt.Sprintf("getUserConnectionDataFromCache() - Error while conversion of data from cache, %s: %s", userCacheKeyName, err.Error()))
+		}
+	}
+
+	return userConnectionData, keyExists
+}
+
+// Internal Method to set User connection data in cache
+func setUserConnectionDataInCache(handlers *FeedHandlers, userId string, communityId int, connectionData interface{}) {
+	userCacheKeyName := getUserConnectionCacheKeyName(userId, communityId)
+	set := handlers.cacheHelper.Set(userCacheKeyName, connectionData, 24*time.Hour)
+	if set.Err() != nil {
+		log.Error(fmt.Sprintf("setUserConnectionDataInCache() - Error while setting data in cache, %s: %s", userCacheKeyName, set.Err()))
+	}
+}
+
+// Interal Method to Add user in connection list for a user
+func addUserInConnectionListInCache(handlers *FeedHandlers, primaryUserId string, communityId int, secondaryUserId string) {
+	primaryUserConnectionData, _ := getUserConnectionDataFromCache(handlers, primaryUserId, communityId)
+	secondaryUserConnectionData, _ := getUserConnectionDataFromCache(handlers, secondaryUserId, communityId)
+
+	primaryUserConnectionData[secondaryUserId] = true
+	secondaryUserConnectionData[primaryUserId] = true
+
+	setUserConnectionDataInCache(handlers, primaryUserId, communityId, primaryUserConnectionData)
+	setUserConnectionDataInCache(handlers, secondaryUserId, communityId, secondaryUserConnectionData)
+}
+
+// Internal Method to Remove user from connection list for a user
+func removeUserInConnectionListInCache(handlers *FeedHandlers, primaryUserId string, communityId int, secondaryUserId string) {
+	primaryUserConnectionData, _ := getUserConnectionDataFromCache(handlers, primaryUserId, communityId)
+	secondaryUserConnectionData, _ := getUserConnectionDataFromCache(handlers, secondaryUserId, communityId)
+
+	delete(primaryUserConnectionData, secondaryUserId)
+	delete(secondaryUserConnectionData, primaryUserId)
+
+	setUserConnectionDataInCache(handlers, primaryUserId, communityId, primaryUserConnectionData)
+	setUserConnectionDataInCache(handlers, secondaryUserId, communityId, secondaryUserConnectionData)
+}
+
+// Internal Method to warm up connection list for a user
+func warmUpConnectionList(handlers *FeedHandlers, userId string, communityId int) {
+	userCacheKeyName := getUserConnectionCacheKeyName(userId, communityId)
+	handlers.cacheHelper.Del(userCacheKeyName)
+
+	getConnections := true
+	page := 1
+	pageSize := 50
+
+	for getConnections {
+		success, userConnections := externalHelpers.FetchUserConnectionsByPage(userId, communityId, page, pageSize)
+
+		if success {
+			if len(userConnections.Connections) > 0 {
+				for _, userConnection := range userConnections.Connections {
+					addUserInConnectionListInCache(handlers, userConnection.User1UUID, communityId, userConnection.User2UUID)
+				}
+
+				page += 1
+			} else {
+				getConnections = false
+			}
+		} else {
+			getConnections = false
+		}
+	}
+}
+
+// Internal Method to Update connection list for a user
+func updateConnectionList(handlers *FeedHandlers, primaryUserId string, communityId int, secondaryUserId string, connected bool) {
+	_, primaryUserCacheKeyExists := getUserConnectionDataFromCache(handlers, primaryUserId, communityId)
+	if !primaryUserCacheKeyExists {
+		warmUpConnectionList(handlers, primaryUserId, communityId)
+	}
+
+	if secondaryUserId != "" {
+		_, secondaryUserCacheKeyExists := getUserConnectionDataFromCache(handlers, secondaryUserId, communityId)
+		if !secondaryUserCacheKeyExists {
+			warmUpConnectionList(handlers, secondaryUserId, communityId)
+		} else {
+			if connected {
+				addUserInConnectionListInCache(handlers, primaryUserId, communityId, secondaryUserId)
+			} else {
+				removeUserInConnectionListInCache(handlers, primaryUserId, communityId, secondaryUserId)
+			}
+		}
+	}
+}
+
+// Exposed Method to Update a Connection
+func (handlers *FeedHandlers) UpdateConnection(c *gin.Context) {
+	// fetch headers and url params
+	headers := utils.GetHeaders(c)
+	userId := c.Param("user_id")
+
+	// validation of api_key
+	communityId := externalHelpers.GetCommunityId(c)
+	if communityId == externalHelpers.DefaultCommunityId {
+		return
+	}
+
+	// validation of request body
+	var updateConnectionRequest requests.UpdateConnectionRequest
+	if err := c.ShouldBindJSON(&updateConnectionRequest); err != nil {
+		utils.GeneralAPIValidationError(c, err.Error())
+		return
+	}
+
+	if updateConnectionRequest.Status == constants.CONNECTION_CONNECTED {
+		updateConnectionList(handlers, headers[utils.HeadersMemberId], communityId, userId, true)
+	} else {
+		updateConnectionList(handlers, headers[utils.HeadersMemberId], communityId, userId, false)
+	}
+
+	// return final response
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+	})
+}
---
+++ internal/api/handlers/connectionFeedBuffer.go
@@ -0,0 +1,140 @@
+package handlers
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+	"github.com/nateshr/likeminds-swarm/internal/services/cache"
+	log "github.com/nateshr/likeminds-swarm/internal/services/logging"
+	"github.com/nateshr/likeminds-swarm/internal/utils"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Internal Method to create cache key name for connection feed buffer
+func getConnectionFeedBufferCacheKeyName(userId string, communityId int) string {
+	return fmt.Sprintf(cache.ConnectionFeedBufferCacheKey, userId, utils.ConvertNumberToString(communityId))
+}
+
+// Internal Method to get User Connection Feed Buffer data from cache
+func getConnectionFeedBufferDataFromCache(handlers *FeedHandlers, userId string, communityId int) (map[string]bool, bool) {
+	connectionFeedBufferData := map[string]bool{}
+	keyExists := false
+
+	// Getting data from cache for user
+	userConnectionFeedCacheKeyName := getConnectionFeedBufferCacheKeyName(userId, communityId)
+	get := handlers.cacheHelper.Get(userConnectionFeedCacheKeyName)
+
+	// If key exists in cache for the User
+	if get.Err() == nil || get.Err() == redis.Nil {
+		keyExists = true
+
+		// Convert the data
+		err := get.Scan(connectionFeedBufferData)
+		if err != nil {
+			log.Error(fmt.Sprintf("getConnectionFeedBufferDataFromCache() - Error while conversion of data from cache, %s: %s", userConnectionFeedCacheKeyName, err.Error()))
+		}
+	}
+
+	return connectionFeedBufferData, keyExists
+}
+
+// Internal Method to set User Connection Feed Buffer data in cache
+func setConnectionFeedBufferDataInCache(handlers *FeedHandlers, userId string, communityId int, connectionFeedBufferData interface{}) {
+	userConnectionFeedCacheKeyName := getConnectionFeedBufferCacheKeyName(userId, communityId)
+	set := handlers.cacheHelper.Set(userConnectionFeedCacheKeyName, connectionFeedBufferData, 24*time.Hour)
+	if set.Err() != nil {
+		log.Error(fmt.Sprintf("setConnectionFeedBufferDataInCache() - Error while setting data in cache, %s: %s", userConnectionFeedCacheKeyName, set.Err()))
+	}
+}
+
+// Interal Method to Add post in Connection Feed Buffer list for a user
+func addPostInConnectionFeedBufferInCache(handlers *FeedHandlers, userId string, communityId int, postId string) {
+	connectionFeedBufferData, _ := getConnectionFeedBufferDataFromCache(handlers, userId, communityId)
+
+	connectionFeedBufferData[postId] = true
+
+	setConnectionFeedBufferDataInCache(handlers, userId, communityId, connectionFeedBufferData)
+}
+
+// Internal Method to Remove post from Connection Feed Buffer list for a user
+func removePostInConnectionFeedBufferInCache(handlers *FeedHandlers, userId string, communityId int, postId string) {
+	connectionFeedBufferData, _ := getConnectionFeedBufferDataFromCache(handlers, userId, communityId)
+
+	delete(connectionFeedBufferData, postId)
+
+	setConnectionFeedBufferDataInCache(handlers, userId, communityId, connectionFeedBufferData)
+}
+
+// Internal Method to warm up Connection Feed Buffer list for a user
+func warmUpConnectionFeedBuffer(handlers *FeedHandlers, userId string, communityId int) {
+	userConnectionFeedCacheKeyName := getConnectionFeedBufferCacheKeyName(userId, communityId)
+	handlers.cacheHelper.Del(userConnectionFeedCacheKeyName)
+
+	userConnectionData, _ := getUserConnectionDataFromCache(handlers, userId, communityId)
+	if len(userConnectionData) == 0 {
+		updateConnectionList(handlers, userId, communityId, "", false)
+	}
+
+	userConnectionFeedFilter := gin.H{
+		"community_id": communityId,
+		"user_id":      userId,
+	}
+
+	userConnectionFeedResults, err := handlers.connectionFeedHelper.FindConnectionFeedHelper(userConnectionFeedFilter, gin.H{})
+	if err != nil {
+		log.Error(fmt.Sprintf("warmUpConnectionFeedBuffer() - Error while fetching connection feed data from DB, %s", err.Error()))
+		return
+	}
+
+	existingPostIds := []primitive.ObjectID{}
+	for _, userConnectionFeedResult := range userConnectionFeedResults {
+		existingPostIds = append(existingPostIds, userConnectionFeedResult.PostId)
+	}
+
+	userConnectionData, _ = getUserConnectionDataFromCache(handlers, userId, communityId)
+	userConnectionIds := []string{}
+
+	for userConnectionId := range userConnectionData {
+		userConnectionIds = append(userConnectionIds, userConnectionId)
+	}
+
+	userPostsFilter := gin.H{
+		"is_deleted":   false,
+		"community_id": communityId,
+		"user_id": gin.H{
+			"$in": userConnectionIds,
+		},
+		"post_id": gin.H{
+			"$nin": existingPostIds,
+		},
+	}
+
+	userPostFilterOptions := addSortingOptions(map[string]interface{}{}, "created_at", OrderTypeDescending)
+
+	// fetch user posts using helper method
+	userPostResults, err := handlers.postHelper.FindPostHelper(userPostsFilter, userPostFilterOptions)
+	if err != nil {
+		log.Error(fmt.Sprintf("warmUpConnectionFeedBuffer() - Error while fetching post data from DB, %s", err.Error()))
+		return
+	}
+
+	for _, userPost := range userPostResults {
+		addPostInConnectionFeedBufferInCache(handlers, userId, communityId, userPost.ID.Hex())
+	}
+}
+
+// Internal Method to Update Connection Feed Buffer list for a user
+func updateConnectionFeedBuffer(handlers *FeedHandlers, userId string, communityId int, postId string, add bool) {
+	_, connectionFeedBufferCacheKeyExists := getConnectionFeedBufferDataFromCache(handlers, userId, communityId)
+	if !connectionFeedBufferCacheKeyExists {
+		warmUpConnectionFeedBuffer(handlers, userId, communityId)
+	} else {
+		if postId != "" && add {
+			addPostInConnectionFeedBufferInCache(handlers, userId, communityId, postId)
+		} else if postId != "" && !add {
+			removePostInConnectionFeedBufferInCache(handlers, userId, communityId, postId)
+		}
+	}
+}
