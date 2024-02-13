e79474d14740cde1f7d5f58cf7b246975ca08289
ankitgarg <ankit.garg@likeminds.community>
2024-02-13 18:29:33 +0530

    Removing top liked comments from cache on like API.

--- internal/services/cache/const.go
+++ internal/services/cache/const.go
@@ -5,4 +5,5 @@ const (
 	UserConnnectionCacheKey      = "connection_list_%s_%s"
 	ConnectionFeedBufferCacheKey = "connection_feed_buffer_%s_%s"
 	InferdoApiFailsCountKey      = "inferdo_api_fails_count_%d"
+	PostTopLikedCommentKey       = "%d_%s_top_liked_comments"
 )
