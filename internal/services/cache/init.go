0948417a32fa96bf0021549ff43d0c8a30a9adf8
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-08-08 01:27:12 +0530

    Changes for load testing on azure

--- internal/services/cache/init.go
+++ internal/services/cache/init.go
@@ -1,16 +1,18 @@
 package cache
 
 import (
+	"crypto/tls"
+
 	"github.com/go-redis/redis/v7"
 	"github.com/nateshr/likeminds-swarm/internal/services/environment"
-
-	log "github.com/nateshr/likeminds-swarm/internal/services/logging"
+	"github.com/nateshr/likeminds-swarm/internal/services/logging"
 )
 
 // InitRedis | initialises a connection pool to redis cache
 func InitRedis() *redis.Client {
 	//Initializing Redis
 	dsn := environment.GoDotEnvVariable("REDIS_DSN")
+	password := environment.GoDotEnvVariable("REDIS_PASSWORD")
 	if len(dsn) == 0 {
 		dsn = "localhost:6379"
 	}
@@ -19,12 +21,18 @@ func InitRedis() *redis.Client {
 		Addr: dsn,
 	})
 
+	serverEnviornment := environment.GoDotEnvVariable("SERVER_ENVIRONMENT")
+	if serverEnviornment == "load" {
+		client.Options().Password = password
+		client.Options().TLSConfig = &tls.Config{MinVersion: tls.VersionTLS12}
+	}
+
 	_, err := client.Ping().Result()
 	if err != nil {
 		panic(err)
 	}
 
-	log.Info("Cache(Redis): Successfully connected and pinged.")
+	logging.Info("Cache(Redis): Successfully connected and pinged.")
 
 	return client
 }
