894c80995cfcf047d4da1c40183f2547674c1e4b
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-09-05 19:39:15 +0530

    Bump application version to 1.29.0

--- cmd/server/likeminds-swarm.go
+++ cmd/server/likeminds-swarm.go
@@ -28,7 +28,7 @@ import (
 )
 
 const (
-	AppVersion     string = "1.28.0" // Application Version
+	AppVersion     string = "1.29.0" // Application Version
 	GinPortAddress string = ":8080"  // Gin Port Address
 )
 
