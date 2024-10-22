d24158c02c3ab8ce04d3fa8fc32ee9926039537a
Shubh LM <123367829+shubh-gupta00@users.noreply.github.com>
2024-10-22 14:10:03 +0530

    [LM-12920] Topic categorisation enhancements (#262)
    * Added support of if_original_topic in postTopics
    
    * updated script for backfillPostTopicsData
    
    * Refactor "is_original_topic" instead of "if_original_topic"

--- internal/entities/postTopics.go
+++ internal/entities/postTopics.go
@@ -8,22 +8,25 @@ import (
 
 // Structure for Post Topics
 type PostTopic struct {
-	ID          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-	CommunityId int                `json:"community_id" bson:"community_id"`
-	PostID      primitive.ObjectID `json:"post_id" bson:"post_id"`
-	TopicID     primitive.ObjectID `json:"topic_id" bson:"topic_id"`
-	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
+	ID              primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	CommunityId     int                `json:"community_id" bson:"community_id"`
+	PostID          primitive.ObjectID `json:"post_id" bson:"post_id"`
+	TopicID         primitive.ObjectID `json:"topic_id" bson:"topic_id"`
+	IsOriginalTopic bool               `json:"is_original_topic" bson:"is_original_topic"`
+	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
 // Exposed Method to Create a New Topic Instance
-func NewPostTopic(postId primitive.ObjectID, topicId primitive.ObjectID, communityId int) PostTopic {
+func NewPostTopic(postId primitive.ObjectID, topicId primitive.ObjectID, IsOriginalTopic bool, communityId int,
+) PostTopic {
 	createdAt := time.Now()
 	return PostTopic{
-		PostID:      postId,
-		TopicID:     topicId,
-		CommunityId: communityId,
-		CreatedAt:   createdAt,
-		UpdatedAt:   createdAt,
+		PostID:          postId,
+		TopicID:         topicId,
+		IsOriginalTopic: IsOriginalTopic,
+		CommunityId:     communityId,
+		CreatedAt:       createdAt,
+		UpdatedAt:       createdAt,
 	}
 }
