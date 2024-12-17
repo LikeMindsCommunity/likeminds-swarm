ce271a7025bfa8073ebf1452c8076fb643633661
aditya-likeminds <141997698+aditya-likeminds@users.noreply.github.com>
2024-12-17 11:34:39 +0530

    [LM-13090] Logging Headers Fix (#276)
    * feat: add header sanitization to logging middleware
    
    - Introduced a new function to sanitize request headers by replacing hyphens with underscores.

--- internal/middlewares/logging.go
+++ internal/middlewares/logging.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -47,6 +48,21 @@ func processRequest(c *gin.Context) interface{} {
 	}
 }
 
+// sanitizeRequestHeaders removes sensitive headers and returns a sanitized copy
+func sanitizeRequestHeaders(requestData gin.H) {
+	if headers, ok := requestData["headers"].(http.Header); ok {
+		headersCopy := make(map[string]string)
+		for key, values := range headers {
+			if len(values) > 0 {
+				// Remove hyphens from header keys
+				sanitizedKey := strings.ReplaceAll(key, "-", "_")
+				headersCopy[sanitizedKey] = values[0]
+			}
+		}
+		requestData["headers"] = headersCopy
+	}
+}
+
 // LoggingMiddleware will log the request and response of API
 func LoggingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -67,6 +83,8 @@ func LoggingMiddleware() gin.HandlerFunc {
 
 			// Updating Request Data
 			data["request"] = processRequest(c)
+
+			sanitizeRequestHeaders(data["request"].(gin.H))
 
 			// Processing request
 			c.Next()
