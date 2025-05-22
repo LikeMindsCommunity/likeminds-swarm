f917af3d2d4a689fd12af958a99402e42489e84b
shobhit-likeminds <shobhit.singh@likeminds.community>
2025-05-22 14:59:41 +0530

    update version to v1.40.0

--- cmd/server/likeminds-swarm.go
+++ cmd/server/likeminds-swarm.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	AppVersion     string = "1.39.4" // Application Version
+	AppVersion     string = "1.40.0" // Application Version
 	GinPortAddress string = ":8080"  // Gin Port Address
 )
 
