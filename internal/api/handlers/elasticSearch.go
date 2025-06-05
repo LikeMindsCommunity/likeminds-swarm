aa0a785413507c808e48b545adae9d8f5fdfe83b
shobhit-likeminds <shobhit.singh@likeminds.community>
2025-06-05 22:08:52 +0530

    hotfix : changed es query to included topic where access key is null

--- internal/api/handlers/elasticSearch.go
+++ internal/api/handlers/elasticSearch.go
@@ -273,7 +273,8 @@ func GetTopicIdsFilterQuery(topicIds []string, communityId int) string {
 	}`, communityId, utils.ParseStringArrayToString(topicIds))
 }
 
-// Exposed method to create topic search query
+// Exposed method to create topic search query.
+// Note : Everytime a new field is added in mongodb, it doesn't cause any problem there but it sometimes causes problem in elastic search because in older documents the newly added field is null. So take care of this
 func GetTopicFilterQuery(page int, pageSize int, searchType string, search string, communityId int, filterIsEnabled bool,
 	isEnabled bool, minPosts int, orderByParams []string, parentTopicId string, isCM bool, memberRole string) string {
 
@@ -354,10 +355,26 @@ func GetTopicFilterQuery(page int, pageSize int, searchType string, search strin
 		accessStringArray = utils.ParseStringArrayToString([]string{enums.ONLY_CM_TOPIC_ACCESS, enums.EVERYONE_TOPIC_ACCESS, ""})
 	}
 
-	accessQuery := fmt.Sprintf(`,{
-		"terms": {
-			"access.keyword": %s
-		}
+	accessQuery := fmt.Sprintf(`,
+	{
+	  "bool": {
+	    "should": [
+	      {
+	        "terms": {
+	          "access.keyword": %s
+	        }
+	      },
+	      {
+	        "bool": {
+	          "must_not": {
+	            "exists": {
+	              "field": "access"
+	            }
+	          }
+	        }
+	      }
+	    ]
+	  }
 	}`, accessStringArray)
 
 	return fmt.Sprintf(`
